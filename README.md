af03e4e736d500626b8e547ffc336ef51035f067
sethreidnz <contact@sethreid.co.nz>
2025-01-23 17:45:04 +1300

    add another button

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ yarn
 yarn dev
 ```
 
-This will run the app and you will see a page with just a single menu with item to open the dialog.
+This will run the app and you will see a page with who buttons. One is to toggle a menu and the other is to open a dialog. The dialog is the same in both cases but the dialog will not open when inside the Menu.
--- src/App.tsx
+++ src/App.tsx
@@ -16,41 +16,51 @@ import {
 
 function App() {
   return (
-    <Menu positioning={{ autoSize: true }}>
-      <MenuTrigger disableButtonEnhancement>
-        <Button>Toggle menu</Button>
-      </MenuTrigger>
+    <>
+      <Menu positioning={{ autoSize: true }}>
+        <MenuTrigger disableButtonEnhancement>
+          <Button>Toggle menu</Button>
+        </MenuTrigger>
 
-      <MenuPopover>
-        <MenuList>
-          <MenuItem>
-            <Dialog>
-              <DialogTrigger disableButtonEnhancement>
-                <Button>Open dialog</Button>
-              </DialogTrigger>
-              <DialogSurface>
-                <DialogBody>
-                  <DialogTitle>Dialog title</DialogTitle>
-                  <DialogContent>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quisquam exercitationem cumque repellendus eaque est dolor
-                    eius expedita nulla ullam? Tenetur reprehenderit aut
-                    voluptatum impedit voluptates in natus iure cumque eaque?
-                  </DialogContent>
-                  <DialogActions>
-                    <Button appearance="primary">Do Something</Button>
-                    <DialogTrigger disableButtonEnhancement>
-                      <Button appearance="secondary">Close</Button>
-                    </DialogTrigger>
-                  </DialogActions>
-                </DialogBody>
-              </DialogSurface>
-            </Dialog>{" "}
-          </MenuItem>
-        </MenuList>
-      </MenuPopover>
-    </Menu>
+        <MenuPopover>
+          <MenuList>
+            <MenuItem>
+              <MyDialog />
+            </MenuItem>
+          </MenuList>
+        </MenuPopover>
+      </Menu>
+
+      <MyDialog />
+    </>
   );
 }
+
+const MyDialog = () => {
+  return (
+    <Dialog>
+      <DialogTrigger disableButtonEnhancement>
+        <Button>Open dialog</Button>
+      </DialogTrigger>
+      <DialogSurface>
+        <DialogBody>
+          <DialogTitle>Dialog title</DialogTitle>
+          <DialogContent>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            exercitationem cumque repellendus eaque est dolor eius expedita
+            nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates
+            in natus iure cumque eaque?
+          </DialogContent>
+          <DialogActions>
+            <Button appearance="primary">Do Something</Button>
+            <DialogTrigger disableButtonEnhancement>
+              <Button appearance="secondary">Close</Button>
+            </DialogTrigger>
+          </DialogActions>
+        </DialogBody>
+      </DialogSurface>
+    </Dialog>
+  );
+};
 
 export default App;
